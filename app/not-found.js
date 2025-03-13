b19a08766ec95bdc5d607db94bdad6baf2002b13
yuuhi <braderyuuhi@gmail.com>
2025-03-13 11:32:33 +0700

    fix not found

--- app/not-found.js
+++ app/not-found.js
@@ -8,7 +8,7 @@ export default function NotFound() {
       <h1 className="text-9xl font-extrabold text-gray-500">404</h1>
       <h2 className="text-3xl font-semibold text-gray-700 mt-4">Page Not Found</h2>
       <p className="text-gray-500 mt-2">The page you are looking for doesn&apos;t exist or has been moved.</p>
-      <Link href="/" className="mt-6 bg-gray-200 hover:bg-gray-300 text-black px-6 py-3 rounded-lg shadow-md inline-block">
+      <Link href="/" className="mt-6 bg-gray-200 hover:bg-gray-300 text-gray-500 hover:text-gray-600 font-extrabold px-6 py-3 rounded-lg shadow-md inline-block">
         Back to Home
       </Link>
     </div>
